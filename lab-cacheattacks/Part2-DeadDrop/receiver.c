bb3394315590320629ca8b4cc2d3396196e87016
duykhuongnguyen <duykhuongnguyen277@gmail.com>
2025-04-12 18:43:13 -0400

    sf

--- lab-cacheattacks/Part2-DeadDrop/receiver.c
+++ lab-cacheattacks/Part2-DeadDrop/receiver.c
@@ -3,27 +3,48 @@
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
 
+
 int main(int argc, char **argv)
 {
-	// Put your covert channel setup code here
+    // 1. Covert Channel Setup
+    uint8_t *buf = mmap(NULL, 2 * 1024 * 1024, 
+                        PROT_READ | PROT_WRITE,
+                        MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE,
+                        -1, 0);
+    if (buf == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
 
-	printf("Please press enter.\n");
+    volatile uint8_t *target = buf + 4096 * 0; // pick one cache set (e.g., set 0)
 
-	char text_buf[2];
-	fgets(text_buf, sizeof(text_buf), stdin);
+    printf("Please press enter.\n");
 
-	printf("Receiver now listening.\n");
+    char text_buf[2];
+    fgets(text_buf, sizeof(text_buf), stdin);
 
-	bool listening = true;
-	while (listening) {
+    printf("Receiver now listening.\n");
 
-		// Put your covert channel code here
+    bool listening = true;
+    while (listening) {
 
-	}
+        uint64_t time = measure_one_block_access_time((uint64_t)target);
 
-	printf("Receiver finished.\n");
+        int received_bit;
+        uint64_t threshold = 20;
 
-	return 0;
+        if (time > threshold) {
+            received_bit = 1;
+        } else {
+            received_bit = 0;
+        }
+
+        printf("%d\n", received_bit);
+
+    }
+
+    printf("Receiver finished.\n");
+
+    return 0;
 }
-
 
--- lab-cacheattacks/Part2-DeadDrop/sender.c
+++ lab-cacheattacks/Part2-DeadDrop/sender.c
@@ -9,37 +9,44 @@
 
 int main(int argc, char **argv)
 {
-  // Allocate a buffer using huge page
-  // See the handout for details about hugepage management
-  void *buf= mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
-  
-  if (buf == (void*) - 1) {
-     perror("mmap() error\n");
-     exit(EXIT_FAILURE);
-  }
-  // The first access to a page triggers overhead associated with
-  // page allocation, TLB insertion, etc.
-  // Thus, we use a dummy write here to trigger page allocation
-  // so later access will not suffer from such overhead.
-  //*((char *)buf) = 1; // dummy write to trigger page allocation
+    void *buf = mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE,
+                     MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB,
+                     -1, 0);
 
+    if (buf == (void*) -1) {
+        perror("mmap() error\n");
+        exit(EXIT_FAILURE);
+    }
 
-  // TODO:
-  // Put your covert channel setup code here
+    volatile uint8_t *target = (uint8_t *)buf + 4096 * 0; // same target as receiver
 
-  printf("Please type a message.\n");
+    printf("Please type a message (only '0' and '1').\n");
 
-  bool sending = true;
-  while (sending) {
-      char text_buf[128];
-      fgets(text_buf, sizeof(text_buf), stdin);
+    bool sending = true;
+    while (sending) {
+        char text_buf[128];
+        fgets(text_buf, sizeof(text_buf), stdin);
 
-      // TODO:
-      // Put your covert channel code here
-  }
+        // Loop through each character the user typed
+        for (int i = 0; text_buf[i] != '\0'; i++) {
+            char c = text_buf[i];
+            if (c == '0' || c == '1') {
+                int bit = c - '0'; // convert '0'/'1' to integer
 
-  printf("Sender finished.\n");
-  return 0;
+                if (bit == 1) {
+                    // Send 1 by hammering memory
+                    for (int j = 0; j < 1000; j++) {
+                        *target;
+                    }
+                }
+                else {
+                    // Send 0 by doing nothing
+                    usleep(500); // 0.5ms pause
+                }
+            }
+        }
+    }
+
+    printf("Sender finished.\n");
+    return 0;
 }
-
-
